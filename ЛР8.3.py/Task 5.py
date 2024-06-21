9ed46dc27e6ef260e87986d82f85c014485d5f03
snacez <sa50_i.n.drozdov@mpt.ru>
2024-06-21 07:54:19 +0300

    Ягон Дон

---
+++ ЛР8.3.py/Task 5.py
@@ -0,0 +1,6 @@
+def calc_stat(listened):
+    total_songs = len(listened)
+    total_duration = sum(listened)
+    return f'Вы прослушали {total_songs} песен общей продолжительностью {total_duration} минут.'
+
+print(calc_stat([189, 148, 210, 144, 174, 158, 163, 189, 227, 198]))
